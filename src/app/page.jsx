1c452901a4c872dd30c51d78746263bc52b1eb69
thiagogilcamargo <thiagogilcamargo@gmail.com>
2023-10-10 22:59:33 -0300

    rotas + image e links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,29 @@
-  export deafaut funcion Home(){
+import Link from "next/link";
+
+function Home() {
+  return (
+    <>
+      <h1>Bem-vindo à Página Inicial</h1>
+     <div>
+      <ul>
+        <li>
+          <Link href="produtos/tenis">Tênis</Link>
+        </li>
+        <li>
+          <Link href="produtos/calca">Calça</Link>
+        </li>
+        <li>
+          <Link href="produtos/blusa">Blusa</Link>
+        </li>
+      </ul>
     
-  }
+
+    <figure>
+      <Image src="/istockphoto.jpg" alt="Prateleira" width={320}
+      height ={320}/>
+    </figure>
+    <div/>
+  );
+}
+
+
