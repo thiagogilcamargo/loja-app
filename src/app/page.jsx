1742bdd67b03f996700718e894ae8a2613fbb552
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-10-15 07:14:38 -0300

    Rotas+Image e Links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,25 @@
+import Image from "next/image";
 import Link from "next/link";
 
-function Home() {
+export default function Home() {
   return (
     <>
-      <h1>Bem-vindo à Página Inicial</h1>
-     <div>
-      <ul>
-        <li>
-          <Link href="produtos/tenis">Tênis</Link>
-        </li>
-        <li>
-          <Link href="produtos/calca">Calça</Link>
-        </li>
-        <li>
-          <Link href="produtos/blusa">Blusa</Link>
-        </li>
-      </ul>
-    
+      <h1>HOME</h1>
 
-    <figure>
-      <Image src="/istockphoto.jpg" alt="Prateleira" width={320}
-      height ={320}/>
-    </figure>
-    <div/>
-  );
+      <div>
+        <ul>
+          <li><Link href="/produtos/calca">CALÇA</Link></li>
+          <li><Link href="/produtos/blusa">BLUSA</Link></li>
+          <li><Link href="/produtos/tenis">TÊNIS</Link></li>
+        </ul>
+
+          <figure>
+            <Image src="/istockphoto.jpg" alt="Prateleira" width={320} height={320}/>
+          </figure>
+
+      </div>
+
+
+    </>
+  )
 }
-
-
