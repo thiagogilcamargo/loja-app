1c5fa2a053d7e2a194adbd75c728b7645c029d52
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-10-17 23:03:49 -0300

    commit da aula

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports =
+{
+images : {
+    remotePatterns:[
+        {
+            protocol :'https',
+            hostname: 'httpstatusdogs.com',
+            port:"",
+            pathmame: "/imag/**"
+        },
+    ],
+  },
+}
---
+++ src/app/produtos/mosfet/[slug]/page.jsx
@@ -0,0 +1,7 @@
+export default function ProdutosSlug ({params}){
+return(
+    <div>
+        <h1> Prudoto: {params.slug}</h1>
+    </div>
+  )
+}
