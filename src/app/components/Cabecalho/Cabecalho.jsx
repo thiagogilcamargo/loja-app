87b5ed8b59049724dbca6b3ff988229983975481
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-10-18 04:32:05 -0300

    Layout local + Layout root + Layout composto.

--- src/app/components/Cabecalho/Cabecalho.jsx
+++ src/app/components/Cabecalho/Cabecalho.jsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-
 export default function Cabecalho() {
   return (
     <header>
-      <nav>
-         <ul>
-            <li><Link href="/produtos/calca">CALÇA</Link></li>
-            <li><Link href="/produtos/camisa">CAMISA</Link></li>
-            <li><Link href="/produtos/tenis">TÊNIS</Link></li>
-         </ul>
-         </nav>
+        <nav>
+        <ul>
+          <li><Link href="/produtos/calca">CALÇA</Link></li>
+          <li><Link href="/produtos/camisa">CAMISA</Link></li>
+          <li><Link href="/produtos/tenis">TÊNIS</Link></li>
+      </ul>
+        </nav>
     </header>
-);
+  )
 }
