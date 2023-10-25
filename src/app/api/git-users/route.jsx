8c765aeac76d01e7660d125ce1c5d181071b5cf8
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-10-24 23:07:46 -0300

    api local - assync -tailwind basicis

---
+++ src/app/api/git-users/route.jsx
@@ -0,0 +1,9 @@
+
+export async function GET(request,{params}){
+
+const response = await fetch (" https://api.github.com/users");
+const users = await response.json();
+
+return NextResponse.json(users);
+
+}
