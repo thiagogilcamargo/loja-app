88b8a5d871898031b71019f4c6197bb980e4f697
thiagogilcamargo <thagogilcamargo@gmail.com>
2023-11-07 22:45:29 -0300

    Login finalizad e estilizand msg de erro  redirect

---
+++ src/app/login/[cad]/page.jsx
@@ -0,0 +1,104 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function LoginUser() {
+
+    //Utilizando o redirecionamento quando estamos no cliente:
+    const router = useRouter();
+
+    const [msgstatus, setMsgStatus] = useState("");
+    const [classLoginMsg, setClassLoginMsg] = useState("");
+
+    //Criando um useState para comportar o usuário:
+    const [usuario, setUsuario] = useState({
+        "email":"",
+        "senha":""
+    });
+
+    useEffect(() => {
+       if(msgstatus == "Login realizado com SUCESSO!"){
+          setClassLoginMsg("login-suc");
+        }else if(msgstatus == "USUÁRIO E OU SENHA INVÁLIDOS!"){
+            setClassLoginMsg("login-err");
+        }else{
+            setClassLoginMsg("login");
+        }
+    }, [msgstatus]);
+    
+    //Função de preenchimento do FORM...
+    const handleChange = (e)=>{
+        //Destructuring
+        const{name, value} = e.target;
+        //Prenchendo o campo, utilizando o useState com SPREAD + OnChange:
+        setUsuario({...usuario,[name]:value});
+    }
+
+    //Função de validação e ENVIO dos dados.
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        
+        try {
+            const response = await fetch("http://localhost:3000/api/base/base-users/0",{
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:  JSON.stringify(usuario)
+            });
+
+            if(response.ok){
+                const status = await response.json();
+
+                if(status.status == true){
+                    setMsgStatus("Login realizado com SUCESSO!");
+                    setTimeout(()=>{
+                        setMsgStatus("");
+                        router.push("/");
+                    },5000);
+                }else{
+                    setMsgStatus("USUÁRIO E OU SENHA INVÁLIDOS!");
+                    setTimeout(()=>{
+                        setMsgStatus("");
+                        setUsuario({
+                            "email":"",
+                            "senha":""
+                        });
+                    },5000);
+                }
+            }
+        } catch (error) {
+      }
+    }
+
+  return (
+    <div>
+        <h1>INFORMAÇÕES DOS USUÁRIOS</h1>
+
+            <h2 className={classLoginMsg}>{msgstatus}</h2>
+
+        <div>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>LOGIN</legend>
+                    <div>
+                        <label htmlFor="idEmail">EMAIL:</label>
+                        <input type="email" name="email" id="idEmail" placeholder="Digite o seu EMAIL:" value={usuario.email} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="idSenha">SENHA:</label>
+                        <input type="password" name="senha" id="idSenha" placeholder="Digite a sua SENHA:" value={usuario.senha} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <button>LOGIN</button>
+                    </div>
+                    <div>
+                        <p>Se você ainda não possui registro. <Link href="/login/cad">CLIQUE AQUI</Link></p>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+  )
+}
